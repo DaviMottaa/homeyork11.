d88b0d40f059359d62cd54e8a9eb2024c61645c9
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-07-15 20:55:57 -0300

    Add files via upload

---
+++ Davi_150721.py
@@ -0,0 +1,37 @@
+#question2
+for item in range(1,21):
+    print(item)
+
+#question3
+print("3*************************")
+
+
+#question4
+print("3***************************")
+alien={"planeta":"vênus","idade":47}
+
+#question5
+print(alien)
+
+#question6
+print("6***************************")
+print(alien["idade"])
+
+#question7
+print("7***************************")
+del alien["planeta"]
+print(alien)
+
+#question8
+print(alien)
+
+#question9
+print("9***************************")
+alien["name"]="E.T Bilu"
+
+#question10
+print(alien)
+
+
+
+
